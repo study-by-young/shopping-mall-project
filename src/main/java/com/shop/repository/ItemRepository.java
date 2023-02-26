5301ee791860bf2b3726fb513b36e175dcbef313
foreveryoun910 <foreveryoun910@gmail.com>
2023-02-26 14:55:39 +0900

    2.6 Spring DATA JPA @Query 어노테이션

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +17,13 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    // 파라미터의 순서를 이용해 변수를 JPQL에 전달하는 경우, ':itemDetail' 대신 첫 번째 파라미터를 전달하겠다는 '?1'이라는 표현 사용
+    // 순서를 이용할 경우 파라미터의 순서가 달라지면 쿼리문이 제대로 동작하지 않을 수 있기 때문에 @Param 어노테이션 사용 권장
+
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+    // 기존 데이터베이스에서 사용하던 쿼리를 그대로 사용해야 하는 경우, @Query의 nativeQuery 속성 사용
 }
