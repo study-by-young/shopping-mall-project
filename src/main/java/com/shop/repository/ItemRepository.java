ae5e0ce3accaa6aed41ad468a714a385f27b502f
foreveryoun910 <foreveryoun910@gmail.com>
2023-02-26 14:21:19 +0900

    2.5 쿼리 메소드

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,6 +3,16 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // JpaRepository는 2개의 제네릭 타입을 사용한다. JpaRepository<엔티티 타입 클래스, 기본키 타입>
+
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
